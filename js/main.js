3e106c45ea87377eaa7e4690f9c4545f60ead09c
bugventure <bugventure@gmail.com>
2014-01-30 17:10:32 +0200

    Vels LTD info page

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(function () {
+	//Header image
+	var url = "./img/plovdiv" + (Math.floor(Math.random() * 5) + 1) + ".jpg",
+		image = $("<img />").on("load", function () {			
+			$("#header .background")
+				.css("backgroundImage", "url(" + url + ")")
+				.addClass("visible");
+		}).attr("src", url);
+
+	//Scroll To
+	$(".scroll").click(function (event) {
+		event.preventDefault();
+		$("html,body").animate({ scrollTop: $(this.hash).offset().top }, 300)
+	});
+
+	//Scroll Spy Refresh
+	$('#navbar').scrollspy();
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+
+	$('.scrollToTop').click(function () {
+		$("html, body").animate({ scrollTop: 0 }, 600);
+		return false;
+	});
+});
