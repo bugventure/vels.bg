44dc7c451e7e6aca30eaa3e5d9685f23abcebd9a
bugventure <bugventure@gmail.com>
2014-01-31 19:22:29 +0200

    maps improvements

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function () {
 	//Header image
 	var url = "./img/plovdiv" + (Math.floor(Math.random() * 5) + 1) + ".jpg",
 		image = $("<img />").on("load", function () {			
-			$("#header .background")
+			$("header .background")
 				.css("backgroundImage", "url(" + url + ")")
 				.css("opacity", "1");
 		}).attr("src", url);
@@ -12,4 +12,25 @@ $(function () {
 		event.preventDefault();
 		$("html,body").animate({ scrollTop: $(this.hash).offset().top }, 300)
 	});
+
+	//GMaps
+	window.gmapsapiload = function () {
+		var coord = new google.maps.LatLng(42.156486, 24.738295);
+
+		var map = new google.maps.Map(document.getElementById("map-canvas"), {
+			center: coord,
+			zoom: 15,
+			disableDefaultUI: true
+		});
+
+		var marker = new google.maps.Marker({
+			position: coord,
+			map: map,
+			icon: './img/marker.png'
+		});
+	}
+
+	setTimeout(function () {
+		$("<script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyD6qm4syOispaR9OvJbEStMaJMC3mjNan4&sensor=false&callback=gmapsapiload' type='text/javascript' async='async' />").appendTo("body");
+	}, 1000);
 });
